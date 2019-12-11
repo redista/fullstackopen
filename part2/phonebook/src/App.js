9b0bb25a1452e77beb0765f735799566c3c3d63b
redista <sealacquire@gmail.com>
2019-12-11 02:54:59 +0000

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,18 +3,23 @@ import HandleNames from './Components/HandleNames.js'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addPerson = (e) => {
     e.preventDefault()
-    console.log(e.target)
     if (!persons.some(n => n.name === newName)) {
       setPersons(persons.concat(
-        {name: newName}
+        {name: newName,
+        number: newNumber}
         ))
-      setNewName('')
+        setNewName('')
+        setNewNumber('')
       }
     else {
       window.alert(`${newName} already exists`)
@@ -23,8 +28,12 @@ const App = () => {
 
   const handleNameChange = (e) => {
     e.preventDefault()
-    console.log(newName)
     setNewName(e.target.value)
+  }
+
+  const handleNumberChange = (e) => {
+    e.preventDefault()
+    setNewNumber(e.target.value)
   }
 
   return (
@@ -32,14 +41,19 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+        name: <input value={newName} onChange={handleNameChange}/>       
+        </div>
+        <div>  
+        number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
       <HandleNames persons={persons} />
+      </div>
     </div>
   )
 }
--- part2/phonebook/src/Components/HandleNames.js
+++ part2/phonebook/src/Components/HandleNames.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
 const HandleName = ({persons}) => (
-    persons.map(name => <p>{name.name}</p>)
+    persons.map(person => <p>{person.name} {person.number}</p>)
 )
 
 
